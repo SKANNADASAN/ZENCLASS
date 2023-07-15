ac7fe9c3bf45f59f5a17f2934c156c08a2097c8e
SKANNADASAN <skanna2494@gmail.com>
2023-07-15 12:56:31 +0530

    zenclass database

---
+++ zenclass.js
@@ -0,0 +1,261 @@
+db.students.insertMany(
+    [
+        {
+            "users":"kannadasan",
+            "codekata":100,
+            "attendance":"present",
+            "topics":"React - Day 1: ES5 vs ES6",
+            "tasks":"React",
+            "company_drives":"zoho",
+            "mentors":"Sanjay",
+            "date":"15/10/2020",
+            "batch":"B45WD"
+    
+        },
+        {
+            "users":"praveen",
+            "codekata":500,
+            "attendance":"present",
+            "topics":"React- Day -2 : React",
+            "tasks":"React components",
+            "company_drives":"accenture",
+            "mentors":"Sanjay",
+            "date":"16/10/2020",
+            "batch":"B45WD"
+        },
+        {
+            "users":"prabhu",
+            "codekata":150,
+            "attendance":"present",
+            "topics":"React- Day -3 : React hooks & states",
+            "tasks":"React hooks & states",
+            "company_drives":"amazon",
+            "mentors":"Sanjay",
+            "date":"17/10/2020",
+            "batch":"B45WD"
+        },
+        {
+            "users":"pradeep",
+            "codekata":145,
+            "attendance":"present",
+            "topics":"React- Day -4 : React components",
+            "tasks":"react hooks",
+            "company_drives":"google",
+            "mentors":"Sanjay",
+            "date":"18/10/2020",
+            "batch":"B45WD" 
+        },
+        {
+            "users":"anitha r",
+            "codekata":158,
+            "attendance":"present",
+            "topics":"React- Day -5 : react hooks",
+            "tasks":"CRUD",
+            "company_drives":"hcl",
+            "mentors":"Sanjay",
+            "date":"19/10/2020",
+            "batch":"B45WD" 
+        },
+        {
+            "users":"venkat",
+            "codekata":140,
+            "attendance":"present",
+            "topics":"React- Day -6 : react router",
+            "tasks":"react with CRUD",
+            "company_drives":"hp",
+            "mentors":"Sanjay",
+            "date":"20/10/2020",
+            "batch":"B45WD"
+        },
+        {
+            "users":"pranesh",
+            "codekata":160,
+            "attendance":"present",
+            "topics":"React- Day -7 : react session practice",
+            "tasks":"react practice",
+            "company_drives":"zoho",
+            "mentors":"Sanjay",
+            "date":"21/10/2020",
+            "batch":"B45WD"
+        },
+        {
+            "users":"shashwath",
+            "codekata":148,
+            "attendance":"present",
+            "topics":"React- Day -8 : Context API",
+            "tasks":"Context API",
+            "company_drives":"adobe",
+            "mentors":"Sanjay",
+            "date":"22/10/2020",
+            "batch":"B45WD"
+        },
+        {
+            "users":"shadhvi",
+            "codekata":159,
+            "attendance":"present",
+            "topics":"React- Day -9 : Hook-useRef, useReducer",
+            "tasks":"components",
+            "company_drives":"ibm",
+            "mentors":"Sanjay",
+            "date":"23/10/2020",
+            "batch":"B45WD"
+        },{
+            "users":"mahathi",
+            "codekata":600,
+            "attendance":"present",
+            "topics":"React- Day -10 : react with CRUD",
+            "tasks":"teachers managemnet",
+            "company_drives":"cisco",
+            "mentors":"Sanjay",
+            "date":"24/10/2020",
+            "batch":"B45WD" 
+        },{
+            "users":"sivabalan",
+            "codekata":140,
+            "attendance":"present",
+            "topics":"React- Day -11 : react practice",
+            "tasks":"react practice",
+            "company_drives":"oracle",
+            "mentors":"Sanjay",
+            "date":"25/10/2020",
+            "batch":"B45WD"  
+        },
+        {
+            "users":"anirutha",
+            "codekata":170,
+            "attendance":"present",
+            "topics":"React- Day -12 : react redux",
+            "tasks":"Redux",
+            "company_drives":"apple",
+            "mentors":"Sanjay",
+            "date":"26/10/2020",
+            "batch":"B45WD" 
+        },
+        {
+            "users":"burhan",
+            "codekata":121,
+            "attendance":"present",
+            "topics":"Database - MySQL",
+            "tasks":"MySql",
+            "company_drives":"intel",
+            "mentors":"Sanjay",
+            "date":"27/10/2020",
+            "batch":"B45WD"  
+        },
+        {
+            "users":"anitha h",
+            "codekata":122,
+            "attendance":"present",
+            "topics":"Database- Day -2 - MySQL",
+            "tasks":"SQLBolt",
+            "company_drives":"salesforce",
+            "mentors":"Sanjay",
+            "date":"28/10/2020",
+            "batch":"B45WD"
+        },{
+            "users":"radhika",
+            "codekata":132,
+            "attendance":"present",
+            "topics":"Database - MongoDB",
+            "tasks":"Database - MongoDB",
+            "company_drives":"VMware",
+            "mentors":"Sanjay",
+            "date":"29/10/2020",
+            "batch":"B45WD"
+        },{
+            "users":"sarath",
+            "codekata":142,
+            "attendance":"present",
+            "topics":"MongoDB",
+            "tasks":"MongoDB",
+            "company_drives":"uber",
+            "mentors":"Sanjay",
+            "date":"30/10/2020",
+            "batch":"B45WD"  
+        },
+        {
+            "users":"bharath",
+            "codekata":146,
+            "attendance":"present",
+            "topics":"Nodejs- Day -1 : Nodejs",
+            "tasks":"Nodejs",
+            "company_drives":"amdocs",
+            "mentors":"Sanjay",
+            "date":"31/10/2020",
+            "batch":"B45WD"  
+        },
+        {
+            "users":"rajesh",
+            "codekata":0,
+            "attendance":"absent",
+            "topics":"Nodejs- Day -2 : Nodejs",
+            "tasks":"no",
+            "company_drives":"no",
+            "mentors":"Mohan",
+            "date":"15/10/2020",
+            "batch":"B46WD"  
+        },{
+            "users":"abdul",
+            "codekata":0,
+            "attendance":"absent",
+            "topics":"Nodejs- Day -3 : Nodejs",
+            "tasks":"no",
+            "company_drives":"no",
+            "mentors":"Mohan",
+            "date":"16/10/2020",
+            "batch":"B46WD"  
+        },
+        {
+            "users":"abdur",
+            "codekata":0,
+            "attendance":"absent",
+            "topics":"Nodejs- Day -4 : Nodejs",
+            "tasks":"no",
+            "company_drives":"no",
+            "mentors":"Mohan",
+            "date":"20/10/2020" ,
+            "batch":"B46WD"  
+        },
+        {
+            "users":"karthick",
+            "codekata":0,
+            "attendance":"absent",
+            "topics":"Nodejs- Day -5 : Nodejs",
+            "tasks":"no",
+            "company_drives":"no",
+            "mentors":"Mohan",
+            "date":"31/10/2020",
+            "batch":"B46WD"  
+        }
+    ]
+)
+
+//Question 1: Find all the topics and tasks which are thought in the month of October
+//Ans:
+db.students.find({date:{$gte:15/10/2020, $lte:31/10/2020}}).sort({date:1})
+
+//Question 2: Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+//Ans:
+db.students.find({date:{$gte:15/10/2020, $lte:31/10/2020}}).sort({date:1})
+
+//Question 3: Find all the company drives and students who are appeared for the placement.
+//Ans:
+db.students.find({$not:{company_drives:"no"}})
+
+//Question 4: Find the number of problems solved by the user in codekata
+//Ans:
+db.students.aggregate([
+  {$match:{batch:'B45WD'}},
+  {$group :{
+    _id:'$users',
+    totalCodekata:{$sum:'$codekata'}
+  }}
+ ])
+
+//Question 5: Find all the mentors with who has the mentee's count more than 15
+//Ans:
+db.students.find({mentors:"Sanjay"})
+
+//Question 6: Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+//Ans:
+db.students.find({attendance:"absent"})
